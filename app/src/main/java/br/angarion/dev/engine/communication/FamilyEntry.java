6316dcf8c73cd65e95c519d365142a71e638bdf3
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-19 07:19:51 -0300

    Use Object2IntMap generic in FamilyEntry

--- app/src/main/java/br/angarion/dev/engine/communication/FamilyEntry.java
+++ app/src/main/java/br/angarion/dev/engine/communication/FamilyEntry.java
@@ -1,5 +1,5 @@
 package br.angarion.dev.engine.communication;
 
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 
-public record FamilyEntry(int id, Object2IntOpenHashMap<String> types) {}
+public record FamilyEntry(int id, Object2IntMap<String> types) {}
