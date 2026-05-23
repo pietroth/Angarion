1c8f0d8c2b6b90c81fabd099fa9f6807c64cb34c
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-23 19:32:48 -0300

    Create communication.intention.IntentionInConstruction

---
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/intention/IntentionInConstruction.java
@@ -0,0 +1,37 @@
+package br.angarion.dev.engine.communication.intention;
+
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.StructLayout;
+import java.lang.foreign.ValueLayout;
+import java.lang.foreign.MemoryLayout.PathElement;
+import java.lang.invoke.VarHandle;
+
+public final class IntentionInConstruction {
+    
+    private static final StructLayout LAYOUT = MemoryLayout.structLayout(
+        ValueLayout.JAVA_INT.withName("originId"),
+        ValueLayout.JAVA_INT.withName("correlationId")
+    ).withByteAlignment(8);
+
+    private static final VarHandle ORIGIN_ID = 
+        LAYOUT.varHandle(PathElement.groupElement("originId"));
+
+    private static final VarHandle CORRELATION_ID = 
+        LAYOUT.varHandle(PathElement.groupElement("correlationId"));
+
+    public static final void writeIntention(MemorySegment dest, int originId, int correlationId) {
+        ORIGIN_ID.set(dest, 0L, originId);
+        CORRELATION_ID.set(dest, 0L, correlationId);
+    }
+
+    public static final int getOriginId(MemorySegment src) {
+        return (int) ORIGIN_ID.get(src, 0L);
+    }
+
+    public static final int getCorrelationId(MemorySegment src) {
+        return (int) CORRELATION_ID.get(src, 0L);
+    }
+
+    public static final long LAYOUT_SIZE = LAYOUT.byteSize();
+}
