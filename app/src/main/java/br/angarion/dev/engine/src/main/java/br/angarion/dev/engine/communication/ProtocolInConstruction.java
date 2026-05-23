296f292d7fe453a5218cbbaedbc206ee9ee48dd0
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-23 20:34:10 -0300

    Rename MIInConstruction to ProtocolInConstruction

---
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/ProtocolInConstruction.java
@@ -0,0 +1,36 @@
+package br.angarion.dev.engine.communication;
+
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.StructLayout;
+import java.lang.foreign.ValueLayout;
+import java.lang.foreign.MemoryLayout.PathElement;
+import java.lang.invoke.VarHandle;
+
+public abstract class ProtocolInConstruction {
+    
+    public static final StructLayout LAYOUT = MemoryLayout.structLayout(
+        ValueLayout.JAVA_INT.withName("totalSize"),
+        ValueLayout.JAVA_INT.withName("id")
+    ).withByteAlignment(8);
+
+    private static final VarHandle TOTAL_SIZE = 
+        LAYOUT.varHandle(PathElement.groupElement("totalSize"));
+
+    private static final VarHandle ID = 
+        LAYOUT.varHandle(PathElement.groupElement("id"));
+
+    public static final void writeMI(MemorySegment dest, int id) {
+        ID.set(dest, 0L, id);
+    }
+
+    public static final int getId(MemorySegment src) {
+        return (int) ID.get(src, 0L);
+    }
+
+    public static final int getTotalSize(MemorySegment src) {
+        return (int) TOTAL_SIZE.get(src, 0L);
+    }
+
+    public static final long LAYOUT_SIZE = LAYOUT.byteSize();
+}
