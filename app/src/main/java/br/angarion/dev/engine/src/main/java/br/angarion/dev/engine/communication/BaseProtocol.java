c9a18f9c0a544f38026534ab039b155f9f79e8e2
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-24 11:22:40 -0300

    Rename ProtocolInConstruction to BaseProtocol and remove protocol Id

--- app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/BaseProtocol.java
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/BaseProtocol.java
@@ -4,8 +4,8 @@ import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.StructLayout;
 import java.lang.foreign.ValueLayout;
 
-public final class ProtocolInConstruction {
-    private ProtocolInConstruction(){}
+public final class BaseProtocol {
+    private BaseProtocol(){}
     
     /*
         The ID is a generic protocol identifier. 
@@ -13,9 +13,8 @@ public final class ProtocolInConstruction {
     */
 
     public static final StructLayout LAYOUT = MemoryLayout.structLayout(
-        ValueLayout.JAVA_INT.withName("totalSize"),
-        ValueLayout.JAVA_INT.withName("id")
-    ).withByteAlignment(8);
+        ValueLayout.JAVA_INT.withName("totalSize")
+    );
 
     public static final long LAYOUT_SIZE = LAYOUT.byteSize();
 }
