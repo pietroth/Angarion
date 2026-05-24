f785e310d73008e2cc2d0741a82b54a2ebd3ad23
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-24 16:35:13 -0300

    Add MBT layout to EventInConstruction

--- app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/event/EventInConstruction.java
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/event/EventInConstruction.java
@@ -8,6 +8,7 @@ import java.lang.foreign.MemoryLayout.PathElement;
 import java.lang.invoke.VarHandle;
 
 import br.angarion.dev.engine.communication.BaseProtocol;
+import br.angarion.dev.engine.communication.MBT;
 
 public final class EventInConstruction {
     private EventInConstruction(){}
@@ -19,6 +20,7 @@ public final class EventInConstruction {
 
     private static final StructLayout LAYOUT = MemoryLayout.structLayout(
         BaseProtocol.LAYOUT.withName("protocol"), // 4 bytes (1 int)
+        MBT.LAYOUT.withName("mbt"), // 4 bytes (1 int)
         ValueLayout.JAVA_INT.withName("originId")
     );
 
@@ -31,8 +33,15 @@ public final class EventInConstruction {
     private static final VarHandle ORIGIN_ID = 
         LAYOUT.varHandle(PathElement.groupElement("originId"));
 
-    public static final void writeHeader(MemorySegment dest, int totalSize, int originId) {
+    private static final VarHandle MBT_ID = 
+        LAYOUT.varHandle(
+            PathElement.groupElement("mbt"),
+            PathElement.groupElement("id")
+        );
+
+    public static final void writeHeader(MemorySegment dest, int typeId, int totalSize, int originId) {
         PROTOCOL_TOTAL_SIZE.set(dest, 0L, totalSize);
+        MBT_ID.set(dest, 0L, typeId);
         ORIGIN_ID.set(dest, 0L, originId);
     }
 
@@ -42,6 +51,10 @@ public final class EventInConstruction {
 
     public static final int getTotalSize(MemorySegment src) {
         return (int) PROTOCOL_TOTAL_SIZE.get(src, 0L);
+    }
+
+    public static final int getTypeId(MemorySegment src) {
+        return (int) MBT_ID.get(src, 0L);
     }
 
     public static final long HEADER_SIZE = LAYOUT.byteSize();
