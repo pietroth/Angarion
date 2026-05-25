3dfecf7f0185957af00e25d26bbe0de24782102d
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-25 07:53:25 -0300

    Rename IntentionInConstruction.getFamily() to getUnpackedFamily()

--- app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/intention/IntentionInConstruction.java
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/communication/intention/IntentionInConstruction.java
@@ -71,7 +71,7 @@ public final class IntentionInConstruction {
         return (int) MBT_ID.get(src, 0L);
     }
 
-    public static final short getFamily(MemorySegment src) {
+    public static final short getUnpackedFamily(MemorySegment src) {
         return MBT.unpackFamily(getTypeId(src));
     }
 
