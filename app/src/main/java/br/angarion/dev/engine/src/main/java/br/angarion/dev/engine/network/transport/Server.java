9e46c85da5c0e751569678a124772140278953fc
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-29 20:01:26 -0300

    Remove Server.notifyConnectionCreated()

--- app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/network/transport/Server.java
+++ app/src/main/java/br/angarion/dev/engine/src/main/java/br/angarion/dev/engine/network/transport/Server.java
@@ -2,6 +2,6 @@ package br.angarion.dev.engine.network.transport;
 
 public interface Server extends Runnable {
     void subscribe(ConnectionCreatedListener listener);
+
     void unsubscribe(ConnectionCreatedListener listener);
-    void notifyConnectionCreated(Connection connection);
 }
--- app/src/main/java/br/angarion/dev/processors/src/main/java/br/angarion/dev/processors/AngarionCompiler.java
+++ app/src/main/java/br/angarion/dev/processors/src/main/java/br/angarion/dev/processors/AngarionCompiler.java
@@ -16,13 +16,12 @@ import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 
 import com.google.auto.service.AutoService;
 import com.palantir.javapoet.FieldSpec;
 import com.palantir.javapoet.TypeName;
 import com.palantir.javapoet.TypeSpec;
-
-import javax.tools.Diagnostic;
 
 import br.angarion.dev.api.communication.DataField;
 import br.angarion.dev.api.communication.FamilyConfiguration;
@@ -34,21 +33,21 @@ import br.angarion.dev.engine.usecase.UseCase;
 
 @AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_25)
-@SupportedAnnotationTypes(
-    {
+@SupportedAnnotationTypes({
         "br.angarion.dev.api.communication.FamilyConfiguration",
         "br.angarion.dev.api.communication.TypeConfiguration",
         "br.angarion.dev.api.communication.DataField",
         "br.angarion.dev.api.communication.GenericValidation",
         "br.angarion.dev.api.communication.ValidationPerFamily"
-    })
+})
 public class AngarionCompiler extends AbstractProcessor {
     private IdentifierGlossary glossary;
-    
+
     private Types typeUtils;
     private Elements elementUtils;
 
-    public AngarionCompiler() {}
+    public AngarionCompiler() {
+    }
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -61,7 +60,7 @@ public class AngarionCompiler extends AbstractProcessor {
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv){
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
         // Family processing
 
@@ -69,16 +68,14 @@ public class AngarionCompiler extends AbstractProcessor {
             FamilyConfiguration configuration = el.getAnnotation(FamilyConfiguration.class);
 
             processingEnv.getMessager().printMessage(
-                Diagnostic.Kind.NOTE,
-                "Processing Family: " + el.getSimpleName()
-            );
+                    Diagnostic.Kind.NOTE,
+                    "Processing Family: " + el.getSimpleName());
 
             for (Element enclosed : el.getEnclosedElements()) {
                 if (enclosed.getKind().isField() && enclosed.getAnnotation(DataField.class) != null) {
                     processingEnv.getMessager().printMessage(
-                        Diagnostic.Kind.NOTE, 
-                        "Found annotated field: " + enclosed.getSimpleName()
-                    );
+                            Diagnostic.Kind.NOTE,
+                            "Found annotated field: " + enclosed.getSimpleName());
                 }
             }
 
@@ -89,71 +86,70 @@ public class AngarionCompiler extends AbstractProcessor {
 
         TypeElement typeElement = elementUtils.getTypeElement("br.angarion.dev.api.communication.Type");
         if (typeElement == null) {
-            processingEnv.getMessager().printError
-                ("br.angarion.dev.api.communication.Type was not found in the classpath");
+            processingEnv.getMessager()
+                    .printError("br.angarion.dev.api.communication.Type was not found in the classpath");
         }
         TypeMirror targetInterfaceType = typeElement.asType();
 
         for (Element el : roundEnv.getElementsAnnotatedWith(TypeConfiguration.class)) {
             TypeElement familyElement = getFamilyElement(el);
             FamilyConfiguration familyConfiguration = familyElement.getAnnotation(FamilyConfiguration.class);
-            String familyName = familyConfiguration != null ? familyConfiguration.value() : "<missing-family-configuration>";
+            String familyName = familyConfiguration != null ? familyConfiguration.value()
+                    : "<missing-family-configuration>";
 
             TypeConfiguration configuration = el.getAnnotation(TypeConfiguration.class);
 
             TypeMirror elementType = el.asType();
 
             if (!typeUtils.isSubtype(elementType, targetInterfaceType)) {
-                processingEnv.getMessager().printError
-                    ("Type class must implement br.angarion.dev.api.communication.Type interface besides br.angarion.dev.api.communication.TypeConfiguration annotation");
+                processingEnv.getMessager().printError(
+                        "Type class must implement br.angarion.dev.api.communication.Type interface besides br.angarion.dev.api.communication.TypeConfiguration annotation");
             }
 
             /*
-                begin DataLayout Builder
-            */
+             * begin DataLayout Builder
+             */
             TypeSpec.Builder dataLayoutBuilder = TypeSpec.classBuilder(configuration.name() + "$DL")
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addSuperinterface(DataLayout.class);
-            
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addSuperinterface(DataLayout.class);
+
             // end Codec Builder
 
             /*
-                begin UseCase Builder
-            */
+             * begin UseCase Builder
+             */
             TypeSpec.Builder useCaseBuilder = TypeSpec.classBuilder(configuration.name() + "$UC")
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addSuperinterface(UseCase.class);
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addSuperinterface(UseCase.class);
 
             // end UseCase Builder
 
             /*
-                begin Validator Builder
-            */
+             * begin Validator Builder
+             */
             TypeSpec.Builder validatorBuilder = TypeSpec.classBuilder(configuration.name() + "$V")
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addSuperinterface(Validator.class);
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addSuperinterface(Validator.class);
 
             // end Validator Builder
 
             processingEnv.getMessager().printMessage(
-                Diagnostic.Kind.NOTE,
-                "Processing Type: " + el.getSimpleName() + " from family: " + familyName
-            );
+                    Diagnostic.Kind.NOTE,
+                    "Processing Type: " + el.getSimpleName() + " from family: " + familyName);
 
             for (Element enclosed : el.getEnclosedElements()) {
                 if (enclosed.getKind().isField() && enclosed.getAnnotation(DataField.class) != null) {
                     processingEnv.getMessager().printMessage(
-                        Diagnostic.Kind.NOTE, 
-                        "Found annotated field: " + enclosed.getSimpleName()
-                    );
+                            Diagnostic.Kind.NOTE,
+                            "Found annotated field: " + enclosed.getSimpleName());
 
                     /*
-                        For each field marked as @DataField, create and add a field in codecBuilder
-                    */
-                    dataLayoutBuilder.addField(FieldSpec.builder(TypeName.get(enclosed.asType()), enclosed.getSimpleName().toString())
-                        .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
-                        .build()
-                    );
+                     * For each field marked as @DataField, create and add a field in codecBuilder
+                     */
+                    dataLayoutBuilder.addField(
+                            FieldSpec.builder(TypeName.get(enclosed.asType()), enclosed.getSimpleName().toString())
+                                    .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
+                                    .build());
                 }
             }
 
@@ -161,8 +157,8 @@ public class AngarionCompiler extends AbstractProcessor {
         }
 
         glossary.snapshot().forEach((key, value) -> {
-            processingEnv.getMessager().printMessage
-                (Diagnostic.Kind.NOTE, "Family name: " + key + "; FamilyId: " + value.id() + ". " + value.types());
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
+                    "Family name: " + key + "; FamilyId: " + value.id() + ". " + value.types());
         });
 
         return true;
@@ -174,7 +170,7 @@ public class AngarionCompiler extends AbstractProcessor {
             Element annotationElement = annotationMirror.getAnnotationType().asElement();
             // Filter only the @TypeConfiguration annotation instance.
             if (!(annotationElement instanceof TypeElement annotationType)
-                || !annotationType.getQualifiedName().contentEquals(TypeConfiguration.class.getCanonicalName())) {
+                    || !annotationType.getQualifiedName().contentEquals(TypeConfiguration.class.getCanonicalName())) {
                 continue;
             }
 
@@ -198,5 +194,5 @@ public class AngarionCompiler extends AbstractProcessor {
 
         throw new IllegalStateException("Unable to resolve family for type: " + typeElement.getSimpleName());
     }
-    
+
 }
