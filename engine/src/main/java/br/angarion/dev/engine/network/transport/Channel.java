8975bf08551ebc28b5bec0dae33d52a889006527
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-30 18:57:19 -0300

    Remove the unsubscribe() methods from Server and Channel, considering that all listeners should be subscribed at initialization and it doesn't make sense to remove this subscription at runtime.

---
+++ engine/src/main/java/br/angarion/dev/engine/network/transport/Channel.java
@@ -0,0 +1,14 @@
+package br.angarion.dev.engine.network.transport;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+
+public interface Channel {
+    MemorySegment read(Arena arena) throws IOException;
+    void send(MemorySegment segment) throws IOException;
+    void close() throws IOException;
+    void subscribe(MessageReceivedListener listener);
+    void setId(int id);
+    int getId();
+}
--- engine/src/main/java/br/angarion/dev/engine/network/transport/Server.java
+++ engine/src/main/java/br/angarion/dev/engine/network/transport/Server.java
@@ -2,5 +2,4 @@ package br.angarion.dev.engine.network.transport;
 
 public interface Server extends Runnable {
     void subscribe(ChannelCreatedListener listener);
-    void unsubscribe(ChannelCreatedListener listener);
 }
