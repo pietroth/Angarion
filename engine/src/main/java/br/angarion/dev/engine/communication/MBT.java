a0827cb306be7c4802f55548ca39729234495227
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-06-02 12:19:30 -0300

    Create ComponentResolver benchmarks

---
+++ engine/src/main/java/br/angarion/dev/engine/communication/MBT.java
@@ -0,0 +1,29 @@
+package br.angarion.dev.engine.communication;
+
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.StructLayout;
+import java.lang.foreign.ValueLayout;
+import java.nio.ByteOrder;
+
+// MBT means Message Based on Types (family + specific type)
+
+public final class MBT {
+    
+    public static final StructLayout LAYOUT = MemoryLayout.structLayout(
+        ValueLayout.JAVA_INT.withName("id").withOrder(ByteOrder.BIG_ENDIAN)
+    );
+
+    public static final long LAYOUT_SIZE = LAYOUT.byteSize();
+
+    public static final int packTypeAndFamily(short type, short family) {
+        return ((family & 0xFFFF) << 16) | (type & 0xFFFF);
+    }
+
+    public static final short unpackType(int id) {
+        return (short) (id & 0xFFFF);
+    }
+
+    public static final short unpackFamily(int id) {
+        return (short) ((id >>> 16) & 0xFFFF);
+    }
+}
