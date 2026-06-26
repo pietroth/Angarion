91ad4fef06348ebb720b5861dbe5f4b28acb3f18
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-06-26 16:05:09 -0300

    Create FAMILY and TYPE varHandles in MBT

--- engine/src/main/java/br/angarion/dev/engine/communication/MBT.java
+++ engine/src/main/java/br/angarion/dev/engine/communication/MBT.java
@@ -1,18 +1,30 @@
 package br.angarion.dev.engine.communication;
 
 import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemoryLayout.PathElement;
 import java.lang.foreign.StructLayout;
 import java.lang.foreign.ValueLayout;
+import java.lang.invoke.VarHandle;
 import java.nio.ByteOrder;
 
 // MBT means Message Based on Types (family + specific type)
 
 public final class MBT {
-    
+
     public static final StructLayout LAYOUT = MemoryLayout.structLayout(
-        ValueLayout.JAVA_SHORT.withName("family").withOrder(ByteOrder.BIG_ENDIAN),
+        ValueLayout.JAVA_SHORT.withName("family").withOrder(
+            ByteOrder.BIG_ENDIAN
+        ),
         ValueLayout.JAVA_SHORT.withName("type").withOrder(ByteOrder.BIG_ENDIAN)
     );
 
     public static final long LAYOUT_SIZE = LAYOUT.byteSize();
+
+    public static final VarHandle FAMILY = LAYOUT.varHandle(
+        PathElement.groupElement("family")
+    );
+
+    public static final VarHandle TYPE = LAYOUT.varHandle(
+        PathElement.groupElement("type")
+    );
 }
