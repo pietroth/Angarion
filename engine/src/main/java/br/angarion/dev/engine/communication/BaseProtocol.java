ee94455ba51e452f985302d50ef4633809613eb9
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-06-26 16:06:23 -0300

    Add TOTAL_SIZE varHandle in BaseProtocol.

--- engine/src/main/java/br/angarion/dev/engine/communication/BaseProtocol.java
+++ engine/src/main/java/br/angarion/dev/engine/communication/BaseProtocol.java
@@ -1,16 +1,25 @@
 package br.angarion.dev.engine.communication;
 
 import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemoryLayout.PathElement;
 import java.lang.foreign.StructLayout;
 import java.lang.foreign.ValueLayout;
+import java.lang.invoke.VarHandle;
 import java.nio.ByteOrder;
 
 public final class BaseProtocol {
-    private BaseProtocol(){}
+
+    private BaseProtocol() {}
 
     public static final StructLayout LAYOUT = MemoryLayout.structLayout(
-        ValueLayout.JAVA_INT.withName("totalSize").withOrder(ByteOrder.BIG_ENDIAN)
+        ValueLayout.JAVA_INT.withName("totalSize").withOrder(
+            ByteOrder.BIG_ENDIAN
+        )
     );
 
     public static final long LAYOUT_SIZE = LAYOUT.byteSize();
+
+    public static final VarHandle TOTAL_SIZE = LAYOUT.varHandle(
+        PathElement.groupElement("totalSize")
+    );
 }
