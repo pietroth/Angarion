5e19a280bd10460aa5c233903862861d50b5f079
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-07-06 18:16:42 -0300

    Remove ValidationResponse

--- engine/src/main/java/br/angarion/dev/engine/communication/validator/Validator.java
+++ engine/src/main/java/br/angarion/dev/engine/communication/validator/Validator.java
@@ -1,10 +1,9 @@
 package br.angarion.dev.engine.communication.validator;
 
-import java.lang.foreign.MemorySegment;
-
 import br.angarion.dev.engine.communication.DataLayout;
+import java.lang.foreign.MemorySegment;
 
 @FunctionalInterface
 public interface Validator<T extends DataLayout> {
-    ValidatorResponse validate(MemorySegment intention);
+    void validate(MemorySegment intention);
 }
--- engine/src/main/java/br/angarion/dev/engine/communication/validator/ValidatorResponse.java
+++
@@ -1,5 +0,0 @@
-package br.angarion.dev.engine.communication.validator;
-
-import java.lang.foreign.MemorySegment;
-
-public class ValidatorResponse {}
