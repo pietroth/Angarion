c4a2bb08b7fffa312f01f1023dc88934ec8ddcea
pietroth <pietrogabrielpelegrini@gmail.com>
2026-08-29 19:31:54 -0300

    Delete IdentifierGlossary and FamilyEntry

--- engine/src/main/java/br/angarion/dev/engine/communication/FamilyEntry.java
+++
@@ -1,8 +0,0 @@
-package br.angarion.dev.engine.communication;
-
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-
-/*
-    This is used for registration of types' identifications.
-*/
-public record FamilyEntry(int id, Object2IntMap<String> types) {}
